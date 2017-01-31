c02e583ed8c6f6922da70e3e030e1b85de3e6d26
resley <resley.rodrigues@originmarkets.com>
2017-01-31 11:42:18 +0000

    Changed app label to notifications

--- pinax/notifications/apps.py
+++ pinax/notifications/apps.py
@@ -5,5 +5,5 @@ from django.utils.translation import ugettext_lazy as _
 class AppConfig(BaseAppConfig):
 
     name = "pinax.notifications"
-    label = "pinax_notifications"
+    label = "notifications"
     verbose_name = _("Pinax Notifications")
